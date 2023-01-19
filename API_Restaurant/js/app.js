113402b040e33f21826ff71093b428025ec3863a
Hyatus <mejia.cristian583@gmail.com>
2023-01-19 02:01:24 -0600

    Calculadora Consumos y propinas PT1

---
+++ API_Restaurant/js/app.js
@@ -0,0 +1,129 @@
+let cliente = {
+    mesa: '',
+    hora:'',
+    pedido: []
+}
+
+
+const categorias = {
+    1:'Comida',
+    2:'Bebidas',
+    3:'Postres'
+}
+
+const btnGuardarCliente = document.querySelector('#guardar-cliente');
+btnGuardarCliente.addEventListener('click', guardarCliente);
+
+
+function guardarCliente(){
+    const mesa = document.querySelector('#mesa').value;
+    const hora = document.querySelector('#hora').value;
+
+    // Validar campos vacíos
+    const camposVacios = [mesa,hora].some( campo => campo === '');
+
+    if(camposVacios){
+        // Verificar si ya hay una alerta
+        const existeAlerta = document.querySelector('.invalid-feedback');
+        if(!existeAlerta){
+            const alerta = document.createElement('div');
+            alerta.classList.add('invalid-feedback','d-block','text-center');
+            alerta.textContent = 'Todos los campos son obligatorios';
+            document.querySelector('.modal-body form').appendChild(alerta);
+            setTimeout(()=>{
+                alerta.remove()
+            },3000);
+        }
+        return;
+    }
+
+    // Asignar datos de mesa y hora al objeto de cliente 
+    cliente = {...cliente, mesa, hora}
+
+    //Ocultar Modal 
+    const modalFormulario = document.querySelector('#formulario');
+    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+    modalBootstrap.hide();
+
+    console.log(cliente);
+
+    // Mostrar las secciones 
+    mostrarSecciones();
+
+
+    //Obtener platillos de la API de JSON Server 
+    obtenerPlatillos();
+}
+
+function mostrarSecciones(){
+    //querySelectorAll porque son más de una
+    const seccionesOcultas = document.querySelectorAll('.d-none');
+    seccionesOcultas.forEach(seccion => seccion.classList.remove('d-none'))
+}
+
+function obtenerPlatillos(){
+    const url = `http://localhost:4000/platillos`;
+
+    fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(resultado => mostrarPlatillos(resultado))
+    .catch(error => console.log(error))
+
+}
+
+function mostrarPlatillos(platillos){
+    const contenido = document.querySelector('#platillos .contenido');
+    
+    platillos.forEach(platillo => {
+        //const {id, nombre, precio, categoria} = platillo;
+
+        const row = document.createElement('div');
+        row.classList.add('row','py-3','border-top');
+
+        const nombre = document.createElement('div');
+        nombre.classList.add('col-md-4');
+        nombre.textContent = platillo.nombre;
+
+        const precio = document.createElement('div');
+        precio.classList.add('col-md-3','fw-bold');
+        precio.textContent = `$${platillo.precio}`;
+
+        const categoria = document.createElement('div');
+        categoria.classList.add('col-md-3');
+        categoria.textContent = `${categorias[platillo.categoria]}`;
+        
+
+        const inputCantidad = document.createElement('input');
+        inputCantidad.type = 'number';
+        inputCantidad.min = 0;
+        inputCantidad.id = `producto-${platillo.id}`;
+        inputCantidad.classList.add('form-control');
+        inputCantidad.value = 0;
+
+        // Funcion que detecta la cantidad y el platillo que se está agregando 
+        inputCantidad.onchange = () => {
+            const cantidad = parseInt(inputCantidad.value);
+            agregarPlatillo({...platillo,cantidad});
+        } 
+           
+        const agregar = document.createElement('div');
+        agregar.classList.add('col-md-2');
+        agregar.appendChild(inputCantidad);
+
+
+        row.appendChild(nombre);
+        row.appendChild(precio);
+        row.appendChild(categoria);
+        row.appendChild(agregar)
+        contenido.appendChild(row);
+
+        //console.log(platillo)
+    })
+
+
+    function agregarPlatillo(producto){
+        console.log('Desde agregar platillo',producto)
+        
+    }
+
+}
